52336d40f43d29795eb02faba8b5215fec9f844c
syytruong <syytruong@gmail.com>
2021-03-03 21:13:28 +0200

    install react router dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      {/*Home*/}
-    </div>
+    <Router>
+      <div className="App">
+        <Switch path="/">
+          <Header />
+          <Home />
+        </Switch>
+
+        <Switch path="/checkout">
+          <Header />
+          <h1>I am the checkout Route</h1>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
