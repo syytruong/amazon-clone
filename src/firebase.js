73cf6b9599238916dcf44d94e5de59aee687f24b
syytruong <syytruong@gmail.com>
2021-03-03 18:43:09 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjgbddWp8BHTlyx1kr2n0oFd_c69lO0yY",
+  authDomain: "clone-a2517.firebaseapp.com",
+  projectId: "clone-a2517",
+  storageBucket: "clone-a2517.appspot.com",
+  messagingSenderId: "105147185389",
+  appId: "1:105147185389:web:eae4d0065baf419f7b74e4",
+  measurementId: "G-W2J8HF6Y81"
+};
