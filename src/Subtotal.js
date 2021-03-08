57097a9a2a562feea272ce00a633262655c748a7
syytruong <syytruong@gmail.com>
2021-03-08 20:43:11 +0200

    implement the number of item according to items added in checkout page

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
+import { useStateValue } from './StateProvider';
 
 function Subtotal() {
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className='subtotal'>
             <CurrencyFormat 
                 renderText={(value) => (
                     <>
                         <p>
-                            Subtotal (0 items): <strong>{` ${value}`}</strong>
+                            Subtotal ({basket.length} items): <strong>{` ${value}`}</strong>
                         </p>
                         <small className="subtotal__gift">
                             <input type="checkbox" />This order contains a gift
